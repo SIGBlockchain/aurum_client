d73840e4a058f96447b9a803586b9c92cd6f1c21
Harshal Patel <harshalpatel165@gmail.com>
2019-10-09 09:42:03 -0500

    updated validrecipfunc 2

--- internal/wallet/wallet.go
+++ internal/wallet/wallet.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 
 	"github.com/SIGBlockchain/aurum_client/internal/constants"
 	"github.com/SIGBlockchain/aurum_client/internal/hashing"
@@ -262,10 +263,11 @@ func UpdateWallet(balance, stateNonce uint64) error {
 	return nil
 }
 
-//validates the size of the recipient
+//ValidRecipLen validates the size of the recipient
 func ValidRecipLen(recipient string) bool {
-	if len(recipient) != 64 {
-		return false
+	matched, _ := regexp.MatchString("^[a-f0-9]{64}$", recipient)
+	if !matched {
+		return false	
 	}
 	return true
 }
